b0800510d6d45cc54f7199d54d37b27e769eb27f
Myroslav Vecheruk <mvecheruk@gmail.com>
2022-06-04 19:54:19 +0300

    2

--- src/components/ContactList/ListItem/ListItem.jsx
+++ src/components/ContactList/ListItem/ListItem.jsx
@@ -1,8 +1,8 @@
-import s from "./list-item.module.scss";
+import s from './list-item.module.css';
 export default function ListItem({ contacts, onClick }) {
-  return contacts.map((e) => (
+  return contacts.map(e => (
     <li className={s.listItem} key={e.id}>
-      {e.name} - {e.number}{" "}
+      {e.name} - {e.number}{' '}
       <button
         className={s.button}
         onClick={() => {
