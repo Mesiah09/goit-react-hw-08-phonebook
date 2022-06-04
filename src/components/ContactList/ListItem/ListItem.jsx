0bddccb859cc19141e194bb395ead88546cbeee6
Myroslav Vecheruk <mvecheruk@gmail.com>
2022-06-04 20:20:24 +0300

    Update ListItem.jsx

--- src/components/ContactList/ListItem/ListItem.jsx
+++ src/components/ContactList/ListItem/ListItem.jsx
@@ -1,4 +1,4 @@
-import s from './list-item.module.css';
+import s from './list-item.module.scss';
 export default function ListItem({ contacts, onClick }) {
   return contacts.map(e => (
     <li className={s.listItem} key={e.id}>
